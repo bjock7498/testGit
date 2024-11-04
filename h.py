ffac955b426fbcde3d2d87e588a8b72749a4bec5
bjock7498 <bjock7498@stevenscollege.edu>
2024-11-04 12:52:48 -0500

    Removing items.

--- h.py
+++
@@ -1,4 +0,0 @@
-
-name = input("Enter your name: ") # user input
-
-print(f"Hello, {name}!") # print Hello, name! using f strings
--- w.go
+++
@@ -1,9 +0,0 @@
-package main // namespace stuff
-
-import ( // import fmt lib
-	"fmt"
-)
-
-func main() {
-	fmt.Println("Hello World") // print "Hello World"
-}
